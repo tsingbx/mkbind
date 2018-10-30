e7a95299b9af3f5a17bd87e022edea3a618066d8
xulingjiao <379559296@qq.com>
2018-10-30 11:11:15 +0800

    add TBMBBind

---
+++ MKBind/MKBind.h
@@ -0,0 +1,20 @@
+//
+//  MKBind.h
+//  MKBind
+//
+//  Created by xulingjiao on 2018/10/30.
+//  Copyright © 2018年 xulingjiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MKBind.
+FOUNDATION_EXPORT double MKBindVersionNumber;
+
+//! Project version string for MKBind.
+FOUNDATION_EXPORT const unsigned char MKBindVersionString[];
+
+#import <MKBind/TBMB_metamacros.h>
+#import <MKBind/TBMBBind.h>
+
+
